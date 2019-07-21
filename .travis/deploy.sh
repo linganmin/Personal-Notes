edc3eb5f46019d3a2ed5901197acae1b3521240a
linganmin <saboran@163.com>
2019-07-21 15:06:07 +0800

    travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,12 +5,10 @@ deploy_env=$1;
 
 if [ $deploy_env == "dev" ]
 then
-  # todo
-
+  echo "dev"
 elif [ $deploy_env == "stgaing" ]
 then
-  # todo
-
+  echo "stagig"
 elif [ $deploy_env == "prod" ]
 then
   
@@ -21,4 +19,10 @@ then
   git pull
 
 remotessh
+
 fi
+
+# echo $deploy_env;
+# if
+# ssh travis@47.96.70.2
+
